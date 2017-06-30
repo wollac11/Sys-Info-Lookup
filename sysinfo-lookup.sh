c1690dddbaa17a8875d2c4c77ed97496d64f1104
Charlie Callow <wollac11@gmail.com>
2017-06-30 15:54:17 +0100

    Updated version string

--- sysinfo-lookup.sh
+++ sysinfo-lookup.sh
@@ -8,7 +8,7 @@ wmic_bin=./DEPENDS/wmic_ubuntu_x64	# Location of WMIC-Client binary
 print_info() {
 	echo "-----------------------
 ----SYS INFO LOOKUP----
-------Version 1.1------
+------Version 1.2b-----
 --Charlie Callow 2017--
 -----------------------
 "
